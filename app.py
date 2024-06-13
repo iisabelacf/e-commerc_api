2265494858b56923433f67528711ad905571ac0d
Isabela Costa Fernandes <isabelacostafernandes@192.168.0.15>
2024-06-13 17:06:51 -0300

    feat: rota de update, listas de produtos, login e logout

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 # Importação
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user,LoginManager, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "minha_chave_123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+CORS(app)
 
 #Modelagem
+#User (id, username, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
+
 #Produto (id, name, price, description)
 
 class Product(db.Model):
@@ -16,8 +29,34 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+#Autenticaçã0
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#criação de rotas de login e logout
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    data.get("username")
+
+    user = User.query.filter_by(username=data.get("username")).first()
+
+    if user and data.get("password") == user.password:
+            login_user(user)
+            return jsonify({"message": "Logged in sucessfully"})
+    
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
+@app.route('/logout', methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout sucessfully"})
+
 #Criação de rotas
 @app.route('/api/products/add', methods=["POST"])
+@login_required
 def add_product():
     data = request.json
     if 'name' in data and 'price'in data:
@@ -28,6 +67,7 @@ def add_product():
     return jsonify({"message": "Invalid product data"}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     #Recuperar o produto da base de dados
     #Verificar se o produto existe
@@ -39,6 +79,54 @@ def delete_product(product_id):
         db.session.commit()
         return jsonify({"message": "Product delete sucessfully"})
     return jsonify({"message": "Product not found"}), 404
+
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message":"Product not found"}), 404
+
+#atualização
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+@login_required
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message":"Product not found"}), 404
+#Para recuperar os dados
+    data = request.json
+    if 'name'in data:
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+
+    if 'description'in data:
+        product.description = data['description']
+
+    db.session.commit()
+    return jsonify({'message': 'Product update successfully'})
+
+#rota de listas de produtos
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    print(products)
+    product_list = []
+    for product in products:
+            product_data = {
+                "id": product.id,
+                "name": product.name,
+                "price": product.price
+            }
+            product_list.append(product_data)
+    return jsonify(product_list)
 
 # Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
 @app.route('/')
